2500f619892fee4f74797ce41f86ee4b0681ee57
Dolores Polito <dolores.polito@gmail.com>
2025-06-03 21:09:54 -0300

    landing hero

--- src/components/LandingV3/index.js
+++ src/components/LandingV3/index.js
@@ -1,70 +1,83 @@
 "use client";
 import styles from "./style.module.scss";
-import {
-  slideUp,
-  slideUp2,
-  slideUp3,
-  slideLeft,
-  fadeInScale,
-} from "./animation";
+import { slideUp, slideLeft, fadeInScale } from "./animation";
 import { motion } from "framer-motion";
 import React from "react";
 import Image from "next/image";
 import completelogo from "../../../public/logo/logo-completo.png";
-import image1 from "../../../public/images/paneles/eo2.jpg";
-import image2 from "../../../public/images/paneles/h4.png";
-import image3 from "../../../public/images/paneles/3...jpg";
-import image4 from "../../../public/images/paneles/2...jpg";
+import image1 from "../../../public/images/newhero/1.png";
+import image2 from "../../../public/images/newhero/2.png";
+import image3 from "../../../public/images/newhero/3.png";
+import image4 from "../../../public/images/newhero/4.png";
+import image5 from "../../../public/images/newhero/5.png";
 
 export default function LandingV3() {
   return (
     <div className={styles.section} id="home">
-      <div className={styles.topcontainer}>
-        <motion.div variants={slideUp2} initial="initial" animate="enter">
-          <Image src={completelogo} alt="logo camara" />
-        </motion.div>
-
+      <div className={styles.gridContainer}>
         <motion.div
-          className={`${styles.aroundImage} ${styles.topLeft}`}
+          className={styles.gridItem}
           variants={slideLeft}
           initial="initial"
           animate="enter"
+          transition={{ delay: 0.2 }}
+          style={{ gridArea: "img1" }}
         >
-          <Image src={image2} alt="Imagen 1" />
+          <Image src={image1} alt="Eólica" layout="responsive" />
         </motion.div>
 
         <motion.div
-          className={`${styles.aroundImage} ${styles.topRight}`}
-          variants={slideUp3}
+          className={styles.gridItem}
+          variants={slideUp}
           initial="initial"
           animate="enter"
+          transition={{ delay: 0.4 }}
+          style={{ gridArea: "img2" }}
         >
-          <Image src={image1} alt="Imagen 2" />
+          <Image src={image2} alt="Paneles solares" layout="responsive" />
         </motion.div>
 
         <motion.div
-          className={`${styles.aroundImage} ${styles.bottomLeft}`}
+          className={styles.gridItem}
           variants={fadeInScale}
           initial="initial"
           animate="enter"
+          transition={{ delay: 0.6 }}
+          style={{ gridArea: "img3" }}
         >
-          <Image src={image3} alt="Imagen 3" />
+          <Image src={image3} alt="Panel vertical" layout="responsive" />
         </motion.div>
 
         <motion.div
-          className={`${styles.aroundImage} ${styles.bottomRight}`}
-          variants={{
-            ...slideUp,
-            enter: {
-              ...slideUp.enter,
-              transition: { ...slideUp.enter.transition, delay: 1.2 },
-            },
-          }}
+          className={styles.gridItem}
+          variants={slideUp}
           initial="initial"
           animate="enter"
-       
+          transition={{ delay: 0.8 }}
+          style={{ gridArea: "img4" }}
         >
-          <Image src={image4} alt="Imagen 4" />
+          <Image src={image4} alt="Atardecer con paneles" layout="responsive" />
+        </motion.div>
+
+        <motion.div
+          className={styles.gridItem}
+          variants={slideUp}
+          initial="initial"
+          animate="enter"
+          transition={{ delay: 1 }}
+          style={{ gridArea: "img5" }}
+        >
+          <Image src={image5} alt="Turbina al amanecer" layout="responsive" />
+        </motion.div>
+
+        <motion.div
+          className={styles.logo}
+          variants={slideUp}
+          initial="initial"
+          animate="enter"
+          transition={{ delay: 1.2 }}
+        >
+          <Image src={completelogo} alt="Logo CERER" />
         </motion.div>
       </div>
     </div>

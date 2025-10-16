f9012a17fc5100d1cfc8885396cf8dcb8a121c30
Dolores Polito <dolores.polito@gmail.com>
2025-10-16 17:41:29 -0300

    nav

--- src/structure/Header/index.js
+++ src/structure/Header/index.js
@@ -5,12 +5,35 @@ import Button from "./Button";
 import styles from "./styles.module.scss";
 import Nav from "./Nav";
 
-const menu = {
+const menuDesktop = {
   open: {
     width: "55vh",
     height: "99vh",
     top: "-1.5vh",
-    right: "-5vh",
+    right: "-3vh",
+    transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] },
+  },
+  closed: {
+    width: "0vh",
+    height: "0vh",
+    top: "0vh",
+    right: "0vh",
+
+    transition: {
+      duration: 0.75,
+      delay: 0.35,
+      type: "tween",
+      ease: [0.76, 0, 0.24, 1],
+    },
+  },
+};
+
+const menuMobile = {
+  open: {
+    width: "50vh",
+    height: "99vh",
+    top: "-1.5vh",
+    right: "-2vh",
     transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1] },
   },
   closed: {
@@ -35,7 +58,7 @@ export default function Index() {
     <div className={styles.header}>
       <motion.div
         className={styles.menu}
-        variants={menu}
+        variants={menuMobile}
         animate={isActive ? "open" : "closed"}
         initial="closed"
       >
